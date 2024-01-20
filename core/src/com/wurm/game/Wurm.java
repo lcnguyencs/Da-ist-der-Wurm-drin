9f23773400d05ea3e10222e5f2654f40ebdacc6c
lcnguyencs <117429230+lcnguyencs@users.noreply.github.com>
2024-01-20 11:09:24 +0100

    update Wurm.java

--- core/src/com/wurm/game/Wurm.java
+++ core/src/com/wurm/game/Wurm.java
@@ -1,31 +1,23 @@
-package com.wurm.game;
+package com.lcnguyencs.src;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import GameState.MenuState;
+import Manager.*;
 
-public class Wurm extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
-
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+public class Wurm extends Game {
+    public static final int V_WIDTH = 1280;
+    public static final int V_HEIGHT = 720;
+    public static SpriteBatch batch;
+    public GameStateManager gsm;
+    @Override
+    public void create () {
+        batch = new SpriteBatch();
+        gsm = new GameStateManager(this);
+        gsm.push(new MenuState(this, gsm)); // Pass the GameStateManager to MenuState
+    }
+    @Override
+    public void render () {
+        super.render();
+    }
 }
