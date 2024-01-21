89e4ccb900b4183bf19b1fd791880c491706193f
Thien Huong Duong <109571399+dxd2057@users.noreply.github.com>
2024-01-22 00:09:58 +0100

    Update Worm.java

--- core/src/Entities/Worm.java
+++ core/src/Entities/Worm.java
@@ -1,5 +1,24 @@
 package Entities;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 public class Worm {
-    
+    public Texture snakeHead;
+    public SpriteBatch batch;
+    public float x;
+    public float y;
+    public String id;
+    public int point;
+    public boolean bonus;
+
+    public Worm(String texturePath, SpriteBatch batch, float x, float y, String id, int point, boolean bonus){
+        this.snakeHead = new Texture(texturePath);
+        this.batch = batch;
+        this.x = x;
+        this.y = y;
+        this.id = id;
+        this.point = point;
+        this.bonus = bonus;
+    }
 }
