018ed795e965b506edd24f4ac4c28f24520c54d8
Thien Huong Duong <109571399+dxd2057@users.noreply.github.com>
2024-01-25 14:25:52 +0100

    Update MenuState.java

--- core/src/GameState/MenuState.java
+++ core/src/GameState/MenuState.java
@@ -10,6 +10,14 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.wurm.game.Wurm;
+
+import Manager.GameStateManager;
 
 public class MenuState implements Screen {
 
@@ -28,7 +36,26 @@ public class MenuState implements Screen {
     public MenuState(Wurm game,  GameStateManager gsm) {
 
         this.gsm = gsm;
-        
+
+        this.game = game;
+        this.batch = game.batch;
+        gamecamMenu = new OrthographicCamera();
+        gamePortMenu = new FitViewport(Wurm.V_WIDTH, Wurm.V_HEIGHT, gamecamMenu);
+
+        mapLoaderMenu = new TmxMapLoader();
+        mapMenu = mapLoaderMenu.load("map/backgroundMenu.tmx");
+        rendererMenu = new OrthogonalTiledMapRenderer(mapMenu);
+        gamecamMenu.position.set(Wurm.V_WIDTH / 2, Wurm.V_HEIGHT / 2, 0);
+
+        playButton = new Texture("img/PlayButtonNew.png");
+        settingButton = new Texture("img/PlayButtonNew.png");
+        exitButton = new Texture("img/PlayButtonNew.png");
+        wood = new Texture("img/wood.png");
+
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(
+                Gdx.files.internal("font/KOMTXT__.ttf")
+        );
+
         FreeTypeFontGenerator.FreeTypeFontParameter param_fontButton = new FreeTypeFontGenerator.FreeTypeFontParameter();
         param_fontButton.size = 40;
         param_fontButton.borderWidth = 1;
