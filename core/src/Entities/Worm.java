050867b9a4d0de6dfa94a0468c8d2a94bac1cdef
Thien Huong Duong <109571399+dxd2057@users.noreply.github.com>
2024-01-27 22:45:50 +0100

    Update Worm.java

--- core/src/Entities/Worm.java
+++ core/src/Entities/Worm.java
@@ -3,7 +3,12 @@ package Entities;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+/**
+ * This class represents a Worm entity in the game. 
+ * It contains the logic for moving the Worm, updating the game state, and rendering the Worm.
+ */
 public class Worm {
+    // Declare necessary variables for the Worm
     public Texture snakeHead;
     public SpriteBatch batch;
     public float x;
@@ -12,6 +17,17 @@ public class Worm {
     public int point;
     public boolean bonus;
 
+    /**
+     * Constructor for the Worm class. It initializes the Worm's properties.
+     *
+     * @param texturePath The path to the texture file for the Worm.
+     * @param batch The SpriteBatch to be used for drawing the Worm.
+     * @param x The x-coordinate of the Worm.
+     * @param y The y-coordinate of the Worm.
+     * @param id The unique identifier of the Worm.
+     * @param point The initial point of the Worm.
+     * @param bonus The bonus status of the Worm.
+     */
     public Worm(String texturePath, SpriteBatch batch, float x, float y, String id, int point, boolean bonus){
         this.snakeHead = new Texture(texturePath);
         this.batch = batch;
@@ -22,6 +38,11 @@ public class Worm {
         this.bonus = bonus;
     }
 
+    /**
+     * Returns the current point of the Worm.
+     *
+     * @return The current point of the Worm.
+     */
     public int getPoint(){
         return this.point;
     }
@@ -29,9 +50,18 @@ public class Worm {
     public void update(){
     }
 
+    /**
+     * Renders the Worm at its current position.
+     */
     public void render(){
         batch.draw(snakeHead, x, y);
     }
+
+    /**
+     * Moves the Worm by a certain step.
+     *
+     * @param step The step to move the Worm.
+     */
     public void move(int step){
         x = x + step * 20;
         point += step;
