936635420d132a5544796b59642ec834b53a3d5b
sumirez <157108736+sumirez@users.noreply.github.com>
2024-01-27 21:59:42 +0100

    Update EndState.java

--- core/src/GameState/EndState.java
+++ core/src/GameState/EndState.java
@@ -1,6 +1,7 @@
 package GameState;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Color;
@@ -16,46 +17,58 @@ import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
-import com.lcnguyencs.src.Wurm;
+import com.wurm.game.Wurm;
 
 import Managers.GameStateManager;
 
+/**
+ * This class represents the end state of the game. 
+ * It contains the logic for handling user input, updating the game state, and rendering the game.
+ */
 public class EndState implements Screen {
+    // Declare necessary variables for the game state
     public Wurm game;
     public SpriteBatch batch;
-
     public float countTime = 0;
     public OrthographicCamera gamecamEnd;
     public Viewport gamePortEnd;
-
     public TmxMapLoader mapLoaderEnd;
     public TiledMap mapEnd;
     public OrthogonalTiledMapRenderer rendererEnd;
     public static float wmVolume = 4.0F;
     public static Music wm;
-    public Texture newgameButton, exitButton;
+    public Texture newgameButton, exitButton, wood;
     public BitmapFont buttonFont;
     GlyphLayout layout = new GlyphLayout();
     private GameStateManager gsm;
+
+    /**
+     * Constructor for the EndState class. It initializes the GameStateManager and other necessary variables.
+     *
+     * @param game The current game instance.
+     * @param gsm The GameStateManager for managing game states.
+     */
     public EndState(Wurm game,GameStateManager gsm) {
         this.gsm = gsm;
         this.game = game;
-        this.batch = game.batch;
+        this.batch = Wurm.batch;
         gamecamEnd = new OrthographicCamera();
         gamePortEnd = new FitViewport(Wurm.V_WIDTH, Wurm.V_HEIGHT, gamecamEnd);
-
+        //load map
         mapLoaderEnd = new TmxMapLoader();
-        mapEnd = mapLoaderEnd.load("map/backgroundEnd.tmx");
+        mapEnd = mapLoaderEnd.load("map/EndState/backgroundMenu.tmx");
         rendererEnd = new OrthogonalTiledMapRenderer(mapEnd);
         gamecamEnd.position.set(Wurm.V_WIDTH / 2, Wurm.V_HEIGHT / 2, 0);
-
+        //load button
         GameStateManager.bgm.pause();
         wm = Gdx.audio.newMusic(Gdx.files.internal("music/winnerSound.mp3"));
         wm.setVolume(0.1F * wmVolume);
         wm.play();
+        //load font
+        newgameButton = new Texture("img/rectangleButtonBig.png");
+        exitButton = new Texture("img/rectangleButton.png");
+        wood = new Texture("img/MenuState/wood.png");
 
-        newgameButton = new Texture("img/PlayButtonNew1.png");
-        exitButton = new Texture("img/PlayButtonNew.png");
         FreeTypeFontGenerator generator = new FreeTypeFontGenerator(
                 Gdx.files.internal("font/KOMTXT__.ttf")
         );
@@ -69,13 +82,17 @@ public class EndState implements Screen {
 
     @Override
     public void show() {
-        // Initialize your End resources here (buttons, backgrounds, etc.)
     }
-
+    /**
+     * Handles user input. If the screen is touched, it prints "Clicked" to the console.
+     *
+     * @param dt The time delta since the last frame.
+     */
     public void handleInput(float dt) {
         if (Gdx.input.isTouched()) {
             System.out.println("Clicked");
         }
+        // Update camera and renderer view
         gamecamEnd.update();
         rendererEnd.setView(gamecamEnd);
     }
@@ -83,42 +100,47 @@ public class EndState implements Screen {
         handleInput(dt);
     }
 
+    /**
+     * Renders the game state.
+     *
+     * @param delta The time delta since the last frame.
+     */
     @Override
     public void render(float delta) {
         update(delta);
         countTime += delta;
-
+        // Clear the screen
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
+        // Render the map
         rendererEnd.render();
-        game.batch.setProjectionMatrix(gamecamEnd.combined);
-
+        Wurm.batch.setProjectionMatrix(gamecamEnd.combined);
+        // Render the buttons
         batch.begin();
-
-        game.batch.draw(newgameButton,770, 350);
-        game.batch.draw(exitButton, 800, 250);
-
+        Wurm.batch.draw(wood,970,50);
+        Wurm.batch.draw(newgameButton,870, 350);
+        Wurm.batch.draw(exitButton, 900, 250);
+        // Render the text
         String newgameText = "New game";
         layout.setText(buttonFont, newgameText);
-        float textX = 770 + (newgameButton.getWidth() - layout.width) / 2;
+        float textX = 870 + (newgameButton.getWidth() - layout.width) / 2;
         float textY = 350 + (newgameButton.getHeight() + layout.height) / 2;
-        buttonFont.draw(game.batch,newgameText,textX,textY);
+        buttonFont.draw(Wurm.batch,newgameText,textX,textY);
 
         String exitText = "Exit";
         layout.setText(buttonFont, exitText);
-        textX = 800 + (exitButton.getWidth() - layout.width) / 2;
+        textX = 900 + (exitButton.getWidth() - layout.width) / 2;
         textY = 250 + (exitButton.getHeight() + layout.height) / 2;
-        buttonFont.draw(game.batch,exitText,textX,textY);
+        buttonFont.draw(Wurm.batch,exitText,textX,textY);
 
 
         batch.end();
-
-        if (Gdx.input.isTouched()) {
+        // Handle user input
+        if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
             int x = Gdx.input.getX();
             int y = Gdx.graphics.getHeight() - Gdx.input.getY();
 
-            if (x >= 770 && x <= 800 + 220 &&
+            if (x >= 870 && x <= 870 + 220 &&
                     y >= 350 && y <= 350 + 70) {
                 if (wm.isPlaying()) {
                     wm.stop();
@@ -127,7 +149,7 @@ public class EndState implements Screen {
                 gsm.push(new PlayState(game, gsm));
             }
 
-            if (x >= 800 && x <= 800 + 160 &&
+            if (x >= 900 && x <= 900 + 160 &&
                     y >= 250 && y <= 250 + 70) {
                 Gdx.app.exit();
             }
@@ -135,6 +157,12 @@ public class EndState implements Screen {
         }
     }
 
+    /**
+     * Resizes the game viewport when the window size changes.
+     *
+     * @param width The new window width.
+     * @param height The new window height.
+     */
     @Override
     public void resize(int width, int height) {
         gamePortEnd.update(width, height);
@@ -151,8 +179,5 @@ public class EndState implements Screen {
 
     @Override
     public void dispose() {
-        //fontEnd.dispose();
-        //batch.dispose();
-        // Dispose of your End resources when they are no longer needed
     }
 }
