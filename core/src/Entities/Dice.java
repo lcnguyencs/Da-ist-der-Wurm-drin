f99f61dd69dffae471d2c901f536c8465e419939
sumirez <157108736+sumirez@users.noreply.github.com>
2024-01-27 22:15:01 +0100

    Update Dice.java

--- core/src/Entities/Dice.java
+++ core/src/Entities/Dice.java
@@ -1,51 +1,100 @@
 package Entities;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.audio.Music;
+import GameState.PlayState;
 
 /**
- * This class represents a Box entity in the game. 
- * It contains the logic for updating and rendering the Box.
+ * This class represents a Dice entity in the game. 
+ * It contains the logic for rolling the dice, updating the game state, rendering the dice, and playing sounds.
  */
-public class Box {
-    // Declare necessary variables for the Box
-    public Texture box;
+public class Dice {
+    // Declare necessary variables for the Dice
+    public Texture[] texture;
     public SpriteBatch batch;
     public float x;
     public float y;
-    public int id;
-    public boolean status;
+    public static int currentNumber = 0;
+    public static int turn = 0;
+    public static float dmVolume = 10.0F;
+    public static Music dm;
 
     /**
-     * Constructor for the Box class. It initializes the Box's properties.
+     * Constructor for the Dice class. It initializes the Dice's properties.
      *
-     * @param texturePath The path to the texture file for the Box.
-     * @param batch The SpriteBatch to be used for drawing the Box.
-     * @param x The x-coordinate of the Box.
-     * @param y The y-coordinate of the Box.
-     * @param id The unique identifier of the Box.
-     * @param status The status of the Box.
+     * @param batch The SpriteBatch to be used for drawing the Dice.
+     * @param x The x-coordinate of the Dice.
+     * @param y The y-coordinate of the Dice.
      */
-    public Box(String texturePath, SpriteBatch batch, float x, float y, int id, boolean status){
-        this.box = new Texture(texturePath);
+    public Dice(SpriteBatch batch, float x, float y){
+        texture = new Texture[6];
+        for (int i = 0; i < 6; i++){
+            texture[i] = new Texture("img/PlayState/dices/Dice " + (i + 1) + ".png");
+        }
         this.batch = batch;
         this.x = x;
         this.y = y;
-        this.id = id;
-        this.status = status;
-
     }
+
     /**
-     * Updates the Box. 
-     * Currently, there is no implementation in this method.
+     * Returns the current turn number.
+     *
+     * @return The current turn number.
+     */
+    public int getTurn(){
+        return turn;
+    }
+
+    /**
+     * Updates the game state based on user input.
      */
     public void update(){
+        if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
+            float mouse_x = Gdx.input.getX();
+            float mouse_y = Gdx.graphics.getHeight() - Gdx.input.getY();
+            System.out.println(mouse_x + " " + mouse_y);
+            if ((mouse_x >= x && mouse_y >= y) && ((mouse_x <= x+texture[0].getWidth()) && (mouse_y <= y + texture[0].getHeight())))
+            {
+                currentNumber = roll() + 1;
+                sound();
+                PlayState.worms[turn].move(currentNumber);
+                turn = (turn + 1) % 4;
+            }
+            for (int i = 0; i < 4; i++){
+                System.out.println("Point of snake " + i + " = " + PlayState.worms[i].getPoint());
+            }
+        }
     }
 
     /**
-     * Renders the Box at its current position.
+     * Renders the Dice at its current position.
      */
     public void render(){
-        batch.draw(box, x, y);
+        batch.draw(texture[(currentNumber + 5) % 6], x, y);
+    }
+
+    /**
+     * Plays the dice rolling sound.
+     */
+    public void sound(){
+        dm = Gdx.audio.newMusic(Gdx.files.internal("music/diceSound.mp3"));
+        dm.setVolume(0.1F * dmVolume);
+        dm.play();
+    }
+
+    /**
+     * Rolls the dice.
+     *
+     * @return The rolled number.
+     */
+    public int roll(){
+
+        return (int)(Math.random() * 6) % 6;
+    }
+
+    public void dispose(){
     }
 }
